01029d7f2ddb109a54c1d6f8411e759972978b8a
Grant McGrath <grantmcgrath@me.com>
2017-08-17 19:08:55 -0400

    Completed project. Added winner page.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const handlebars = require("express-handlebars");
 const fs = require("fs");
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 const bp = require("body-parser");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -27,9 +28,11 @@ app.use(
 
 // Parses data
 app.use(bp.json());
-app.use(bp.urlencoded( {
+app.use(bp.urlencoded({
   extended: false
 }));
+
+app.use(morgan("dev"));
 
 app.use((req, res, next) => {
   if (!req.session.word) {
@@ -38,102 +41,104 @@ app.use((req, res, next) => {
   next();
 });
 
-let word;
-let wordArray;
-let emptyArray = [];//empty array for initial setup
-let spentCharArray = [];
-let remTurns = 8;
+let word = 0;
+let lettersArray;
+let wordGuess = [];
+let wrongLetters = [];
+let guesses = 8;
+let winner;
 
-app.get('/', function(req, res) {
+app.get("/", function(req, res) {
   if (req.session.word.length === 0) {
-    word = words[Math.floor(Math.random() * words.length)]; //selects random word from library
-    wordArray = (word.toUpperCase()).split(""); //splits random word into array
-    req.session.word = wordArray;
+    // Randomly selects a word from the computer dictionay.
+    word = words[Math.floor(Math.random() * words.length)];
+    console.log(word);
+    // Splits up said random word into individual leters and places them into the array.
+    lettersArray = (word.toUpperCase()).split("");
+    req.session.word = lettersArray;
     for (var i = 0; i < word.length; i++) {
-      emptyArray.push("_"); //creates empty array of same length as word
+      wordGuess.push("_");
+      console.log(wordGuess);
     }
   }
 
-  res.render('home', {
-    emptyArray: emptyArray,
-    remTurns: remTurns
+  res.render("home", {
+    wordGuess: wordGuess,
+    guesses: guesses
   })
 });
 
-app.post("/charGuess", function(req, res) {
+app.post("/guess", function(req, res) {
 
-  let character = req.body.character.toUpperCase();
+  letter = req.body.letter.toUpperCase();
+  console.log(letter);
 
-  req.checkBody('character', '- Please enter a character ').notEmpty();
-  req.checkBody('character', '- Only 1 character allowed').len(1, 1);
+  req.checkBody("letter", "Please enter a letter ").notEmpty();
+  req.checkBody("letter", "Only 1 letter allowed").len(1, 1);
 
   req.getValidationResult()
 
     .then((result) => {
-      // do something with the validation result -
-        // throw error method:
-    if (!result.isEmpty()) {
-      throw new Error(result.array().map((item) => item.msg).join(' - '));
-        } else if (spentCharArray.includes(character)) {
-        throw new Error('Letter has already been guessed');
+      if (!result.isEmpty()) {
+        throw new Error(result.array().map((item) => item.msg).join(" - "));
+      } else if (wrongLetters.includes(letter)) {
+        throw new Error("That letter has already been guessed. Please try again.");
       } else {
-        console.log('No errors')
+        console.log("No errors")
       }
     })
 
     .then(() => {
-      //if guessed letter is present run this block:
-      if (wordArray.includes(character)) {
-        if (!spentCharArray.includes(character)) {
-          spentCharArray.push(character);
+      //If guessed letter is present run this block:
+      if (lettersArray.includes(letter)) {
+        if (!wrongLetters.includes(letter)) {
+          wrongLetters.push(letter);
         }
-        for (var i = 0; i < wordArray.length; i++) {
-          if (character === wordArray[i]) {
-            emptyArray.splice(i, 1, character);
-            console.log(emptyArray);
+        for (var i = 0; i < lettersArray.length; i++) {
+          if (letter === lettersArray[i]) {
+            wordGuess.splice(i, 1, letter);
           }
         }
-        //setup link to winpage
-        if (wordArray.join("").toString() === emptyArray.join("").toString()) {
-           return res.render('winner', {
+        //If player wins.
+        if (lettersArray.join("").toString() === wordGuess.join("").toString()) {
+          return res.render("winner", {
             word: word
           });
         }
       }
-      //if guessed letter is not present
+      //If guessed letter is incorrect.
       else {
-        remTurns--;
-        spentCharArray.push(character);
-        if (remTurns === 0) {
-          return res.render('loser', {
-            word: word
-          })
+        guesses--;
+        wrongLetters.push(letter);
+        // If no guesses remain.
+        if (guesses === 0) {
+          console.log(wrongLetters);
         }
       }
-      res.render('home', {
-        emptyArray: emptyArray,
-        spentCharArray: spentCharArray,
-        remTurns: remTurns
+      res.render("home", {
+        wordGuess: wordGuess,
+        wrongLetters: wrongLetters,
+        guesses: guesses
       });
     })
 
     .catch((error) => {
-      res.render('home', {
-        emptyArray: emptyArray,
+      res.render("home", {
+        wordGuess: wordGuess,
         error: error,
-        spentCharArray: spentCharArray,
-        remTurns: remTurns
+        wrongLetters: wrongLetters,
+        guesses: guesses
       });
     })
 });
 
-app.post("/reset", (req, res) =>{
+app.post("/reset", (req, res) => {
   req.session.word = [];
-    emptyArray = [];
-    spentCharArray = [];
-    remTurns = 8;
-    res.redirect("/");
+  wordGuess = [];
+  wrongLetters = [];
+  guesses = 8;
+  res.redirect("/");
 });
 
 
-app.listen("3000", () => console.log("Port 3000 listening."));
+app.listen("3000", () => console.log("Port 3000 is always listening."));
